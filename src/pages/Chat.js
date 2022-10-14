71ae1f3cef5107d484f7e9f94961a54f988ccb91
Zain Fiaz <106686424+zain12321@users.noreply.github.com>
2022-10-14 17:43:11 +0500

    changes

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -145,6 +145,75 @@ className='btn-box-w'  onClick={() => filterItem('Muhammad Mohsin')}
 	time="06:11pm"
 	className=''/>	
 		</button>
+		<button className='btn-box-w' exact onClick={() => filterItem('Attendents')}>
+	<Sidebar 
+	image="https://pps.whatsapp.net/v/t61.24694-24/299350061_2122296387962552_8776200068908557247_n.jpg?ccb=11-4&oh=01_AVwu_8Af0lwrTpNGvyMRVgjmEOY-lZsyvuumqubl9tGSAw&oe=63550871"
+	name="Attendents"
+	message="Mohsin: 0 out"
+	time="10:00pm"
+	className='flex'/>
+	</button>
+	<button className='btn-box-w' onClick={() => filterItem('Chit Chat')}>
+	<Sidebar 
+	image="https://media-exp1.licdn.com/dms/image/C560BAQEXwbulSJEOwQ/company-logo_100_100/0/1543481551791?e=1669852800&v=beta&t=RpdW9gFwZceJTAM_yFDjV9aPgkXgp5CaofjKWacbwg8"
+	name="Chit Chat"
+	message="Haris: Ameen"
+	time="09:23pm"
+	className=''/>
+	</button>
+	<button className='btn-box-w'  onClick={() => filterItem('Hassan Mujtaba')}>
+	<Sidebar 
+	exact
+	image="https://pps.whatsapp.net/v/t61.24694-24/294942294_349098890749265_2607315862855786782_n.jpg?ccb=11-4&oh=01_AVwcXY92DG5GTl_qiYxpV2pR-5lpIPj3_JEhakx8pDHxGg&oe=6353951F"
+	name="Hassan Mujtaba"
+	message="You: Walaikumassalam"
+	time="07:40pm"
+	className=''/>	
+	</button>
+	<button className='btn-box-w'  onClick={() => filterItem('Mmuhammad Hassan')}>
+	<Sidebar 
+	image="https://pps.whatsapp.net/v/t61.24694-24/302011825_791584021994328_3988356719852908480_n.jpg?ccb=11-4&oh=01_AVyzjbRRSI4gpyLSOAsFc196XuxTFvrDq7iPz63rGpr--g&oe=63537A3B"
+	name="Muhammad Hassan"
+	message="You: Yes"
+	time="02:45pm"
+	className=''/>
+	</button>
+		<button
+		className='btn-box-w'  onClick={() => filterItem('Zubair Butt')}>
+		<Sidebar 
+	image="https://pps.whatsapp.net/v/t61.24694-24/266945871_784062612869051_8230589337531865786_n.jpg?ccb=11-4&oh=01_AVx75ZO4Z_grtrpfZr9K-ad-rfVYISKKskRrLn8FZH-FNw&oe=63503BA1"
+	name="Zubair Butt"
+	message="You: I'm Fine"
+	time="01:23pm"
+	className=''/>
+		</button>
+<button
+className='btn-box-w'  onClick={() => filterItem('Muhammad Mohsin')}
+>
+<Sidebar 
+	image="https://media-exp1.licdn.com/dms/image/C5103AQH5yB-RUqugPg/profile-displayphoto-shrink_100_100/0/1521962589360?e=1666828800&v=beta&t=vbp8Iv5Z82ZK-1Ezr37nmHWPY3ezmGPt-5myo-TFOM4"
+	name="Muhammad Mohsin"
+	message="You: I'm fine"
+	time="10:23am"
+	className=''/></button>
+		<button className='btn-box-w'  onClick={() => filterItem('Shaista Kalsoom')}>
+		<Sidebar 
+	image="https://media-exp1.licdn.com/dms/image/C5103AQGpA2OZUMTiZA/profile-displayphoto-shrink_100_100/0/1575450592914?e=1666828800&v=beta&t=LtdZYtr0D40I48Esfa1bZqISFuMORRzToffG0U-DBsM"
+	name="Shaista Kalsoom"
+	message="You: Hi Zain"
+	time="10:00pm"
+	className=''/>
+		</button>
+		<button
+		className='btn-box-w'  onClick={() => filterItem('Tayyab Sir')}
+		>
+		<Sidebar 
+	image="https://pps.whatsapp.net/v/t61.24694-24/254816431_2882568472043633_8825022336389735842_n.jpg?ccb=11-4&oh=01_AVyqhbHvcfGaVtk7JikIrctSk3sciGtE3Df4th33Uv_GOQ&oe=63537ED9"
+	name="Tayyab Syed"
+	message="You: Hi, Sir"
+	time="06:11pm"
+	className=''/>	
+		</button>
 	</p>
 				</details>
 			</div>
